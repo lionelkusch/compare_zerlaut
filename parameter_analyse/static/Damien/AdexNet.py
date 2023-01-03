a13536c9696facce798528cfdb3696ad29d1a86e
Kusch Lionel <lionel.kusch@univ-amu.fr>
2023-01-03 13:18:46 +0100

    small modification

--- parameter_analyse/static/Damien/AdexNet.py
+++ parameter_analyse/static/Damien/AdexNet.py
@@ -1,3 +1,5 @@
+#  Copyright 2021 Aix-Marseille Université
+# "Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements; and to You under the Apache License, Version 2.0. "
 
 # import libraries
 import matplotlib.pyplot as plt
