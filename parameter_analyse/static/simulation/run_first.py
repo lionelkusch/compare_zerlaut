9f3da969a7fa1e1719fa679c47b2b2402686a3fd
Kusch Lionel <lionel.kusch@univ-amu.fr>
2023-03-15 18:04:09 +0100

    Add new simulation

--- parameter_analyse/static/simulation/run_first.py
+++ parameter_analyse/static/simulation/run_first.py
@@ -36,12 +36,12 @@ from parameter_analyse.static.python_file.parameters import parameter_default
 #                        {'b': np.concatenate([[0.]]), 'rate': np.array(range(100), dtype=float)},
 #                        1000.0, 5000.0, analyse=True, simulation=True)
 #
-for master_seed in range(0, 30):
-    master_seed = int(master_seed)
-    path = os.path.dirname(os.path.realpath(__file__)) + '/master_seed_' + str(master_seed) + '/'
-    parameter_default.param_nest['local_num_threads'] = 8
-    parameter_default.param_nest['master_seed'] = master_seed
-    run_exploration_2D(path, parameter_default, path + 'database.db', 'first_exploration',
-                       {'b': np.concatenate([[60.]]), 'rate': np.array(range(100), dtype=float)},
-                       1000.0, 5000.0, analyse=True, simulation=True)
+# for master_seed in range(0, 30):
+#     master_seed = int(master_seed)
+#     path = os.path.dirname(os.path.realpath(__file__)) + '/master_seed_' + str(master_seed) + '/'
+#     parameter_default.param_nest['local_num_threads'] = 8
+#     parameter_default.param_nest['master_seed'] = master_seed
+#     run_exploration_2D(path, parameter_default, path + 'database.db', 'first_exploration',
+#                        {'b': np.concatenate([[60.]]), 'rate': np.array(range(100), dtype=float)},
+#                        1000.0, 5000.0, analyse=True, simulation=True)
 
